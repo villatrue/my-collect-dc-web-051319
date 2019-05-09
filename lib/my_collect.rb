168b4d9c323ded49852ea2f0d2c1748660c07f2b
Sean Carter <scarter0313@gmail.com>
2019-05-09 01:55:44 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,16 +1,11 @@
 
 
 # module Enumerable
-#   def kollect
-#     new_ary = []
-#     self.each do |elem|
-#       new_ary << yield(elem)
-#     end
-#     new_ary
-#   end
+  def my_collection
+    new_ary = []
+    self.each do |elem|
+      new_ary << yield(elem)
+    end
+    new_ary
+  end
 
-def mycollect
-  i=0
-  empty_array =[]
-  self.each do [i]
-end
